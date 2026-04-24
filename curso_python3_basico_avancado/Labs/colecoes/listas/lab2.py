9a0d00cb43abee85855a6ce3df078cfc8fef1f67
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-04-23 23:33:25 -0300

    (lab) lista metodos da lista e seus usos lab1 e lab2

---
+++ curso_python3_basico_avancado/Labs/colecoes/listas/lab2.py
@@ -0,0 +1,15 @@
+# entend na lista
+
+lista_a = [1, 2, 3]
+lista_b = [4, 5, 6]
+
+# Há duas maneira de se fazer isso
+
+# Concatenando +
+lista_c = lista_a + lista_b
+print(lista_c)
+
+# Concatenando com extend
+print(lista_a)
+lista_a.extend(lista_b)
+print(lista_a)
