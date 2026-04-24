956540fa09d0b40de5d292314c523fc0194ed0fb
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-04-23 23:46:58 -0300

    (aula43) lista referencia de valores na memoria

--- curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula43_dados_mutaveis.py
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula43_dados_mutaveis.py
@@ -4,9 +4,21 @@ Cuidados com dados mutáveis
 = - aponta para o mesmo valor na memória (mutável)
 """
 
-name = 'Paulo'
-last_name = name
-name = 'Paulo Carvalho'
-print(name)
-print(last_name)
+# name = 'Paulo'
+# last_name = name
+# name = 'Paulo Carvalho'
+# print(name)
+# print(last_name)
 
+"""
+Para deixa mais claro
+quando o valor for imutavel você esta copiando o valor para outra variável
+
+quando o valor for mutavel você apenas esta referenciando o valor na memória
+"""
+
+lista_a = ['Luiz', 'Maria']
+lista_b = lista_a
+
+lista_a[0] = 'Qualquer coisa'
+print(lista_b)
