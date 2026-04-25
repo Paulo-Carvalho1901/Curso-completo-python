3e26c79ad958c0c9e20409c1c947d6a431edfc8e
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-04-25 00:22:28 -0300

    (for) iterando com for em uma lista de nomes

---
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula44_for_tipo_list.py
@@ -0,0 +1,11 @@
+"""
+for in com listas
+
+como as lista é um iterável utilizaremos o for
+para iterar sobre cada elemento dessa lista
+"""
+
+lista = ['Maria', 'Helena', 'Luiz']
+
+for nome in lista:
+    print(nome, type(nome))
