1786502b36477c215404b0c742a6e56c7b44ae61
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-04-29 16:16:12 -0300

    (Doc) explicação basica da tupla

---
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula46_tuplas.py
@@ -0,0 +1,9 @@
+# Tipo tupla - uma lista imutável
+
+"""
+Geralmente utilizamos as tuplas:
+
+    Caso que você não precise alterar nemhum dados na 
+    lista se utiliza tuplas pois é um estrutura de dados
+    imutável 
+"""
