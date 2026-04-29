32c0a6208a59f12629c9c348d5d8c70886dece0b
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-04-29 16:06:30 -0300

    (doc) add comentario para estudo futuro

--- curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula45_empacotamento_desempacotamento.py
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula45_empacotamento_desempacotamento.py
@@ -2,5 +2,25 @@
 Introdução ao desempacotamento + tuples (tuplas)
 """
 
+"""
+nomes = ['Maria', 'Helena','Luiz']
+
+formas de desempacotar
+nome1, nome2, nome3 = nomes
+
+nome1, nome2, nome3 = ['Maria', 'Helena','Luiz']
+
+pegando apenas um valor de cada vez do pacote
+
+_, _, nome3 = ['Maria', 'Helena','Luiz']
+_, nome2, _ = ['Maria', 'Helena','Luiz']
+nome1, _, _ = ['Maria', 'Helena','Luiz']
+
+variavel resto cria um novo pacote dos valores que não
+estiver sendo utilizado
+nome1, *resto = ['Maria', 'Helena','Luiz']
+
+"""
+
 _, _, nome3 = ['Maria', 'Helena','Luiz']
 print(nome3)
