e56098be63460e5e836aef34979bedc77abe889b
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-04-29 18:02:39 -0300

    (Doc) enunciado do problema de listas

---
+++ curso_python3_basico_avancado/exercicios/ex13.py
@@ -0,0 +1,8 @@
+"""
+Faça uma lista de compras com listas
+o usúario deve ter a a possibilidade de 
+inserir, apagar, e listar valores da sua lista
+não permita que o programa quebre com
+erros de indices inesistentes na lista
+"""
+
