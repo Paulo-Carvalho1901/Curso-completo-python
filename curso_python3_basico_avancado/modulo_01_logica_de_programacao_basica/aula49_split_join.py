ade246f69522d9d13381716c554a7dd2f53f8142
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-05-07 12:17:57 -0300

    (aula49) utilizando strip para remover os espaços da lista

--- curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula49_split_join.py
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula49_split_join.py
@@ -6,9 +6,11 @@ strip - corta os espaços no inicio e fim da frase
 """
 
 frase = '               Olha só que           , coisa interessante                 '
-lista_de_frases = frase.split(',')
+lista_de_frases_cruas = frase.split(',')
 
-for i, frases in enumerate(lista_de_frases):
-    lista_de_frases[i] = lista_de_frases[i].strip()
+listas_frases = []
+for i, frases in enumerate(lista_de_frases_cruas):
+    listas_frases.append(lista_de_frases_cruas[i].strip())
 
-print(lista_de_frases)
+print(lista_de_frases_cruas)
+print(listas_frases)
