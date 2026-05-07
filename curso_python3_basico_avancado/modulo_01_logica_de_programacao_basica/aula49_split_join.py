b4a41c30ec81c7fd3369fe9b2a83231572ffe85f
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-05-07 12:11:42 -0300

    (aula49) utilizando o strip para remover os espaços da frase

--- curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula49_split_join.py
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula49_split_join.py
@@ -2,8 +2,13 @@
 split e join com list e str
 split - divide uma string
 join - une uma string
+strip - corta os espaços no inicio e fim da frase
 """
 
-frase = 'Olha só que, coisa interessante'
-lista_de_palavras = frase.split(',')
-print(lista_de_palavras)
+frase = '               Olha só que           , coisa interessante                 '
+lista_de_frases = frase.split(',')
+
+for i, frases in enumerate(lista_de_frases):
+    lista_de_frases[i] = lista_de_frases[i].strip()
+
+print(lista_de_frases)
