580fdfa8c1e03aad9ea4ced90dfe015604db0200
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-05-07 11:51:14 -0300

    (aula48) terceira forma com decimal, utilizado para casos com numeros muitos grandes e para uma maior precisão no calculo

--- curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula48_imprecisao_numeros.py
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula48_imprecisao_numeros.py
@@ -4,8 +4,11 @@ Double-precision floating-point format IEEE 754
 https://en.wikipedia.org/wiki/Double-precision_floating-point_format
 https://docs.python.org/pt-br/3/tutorial/floatingpoint.html
 """
-numero_1 = 0.1
-numero_2 = 0.7
+import decimal
+
+numero_1 = decimal.Decimal('0.1')
+numero_2 = decimal.Decimal('0.7')
+
 numero_3 = numero_1 + numero_2
 print(numero_3)
 # Formatando a saída desejada 
@@ -13,4 +16,4 @@ print(numero_3)
 print(f'{numero_3:.2f}')
 
 # Forma 2
-print(round(numero_3, 3))
+print(round(numero_3, 2))
