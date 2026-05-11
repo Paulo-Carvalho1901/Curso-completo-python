f63068f0bc14f26fbd00689b1c95293cea33251d
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-05-11 18:01:15 -0300

    (aula50) listas dentro de listas em Python

---
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula50_listas_dentro_listas.py
@@ -0,0 +1,12 @@
+# Listas de listas e seus indices
+
+salas = [
+    # 0        1 
+    ['Maria', 'Helena', ], # 0
+    # 0
+    ['Elaine', ], # 1
+    # 0       1       2
+    ['Luiz', 'João', 'Eduardo', ], # 2 
+]
+
+print(salas)
