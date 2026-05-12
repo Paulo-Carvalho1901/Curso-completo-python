fd3b0a4272589efeb88a6fcc6c956688337f7a80
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-05-12 11:12:10 -0300

    (aula50) iterando sobre os alunos

--- curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula50_listas_dentro_listas.py
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula50_listas_dentro_listas.py
@@ -6,10 +6,12 @@ salas = [
     # 0
     ['Elaine', ], # 1
     # 0       1       2
-    ['Luiz', 'João', 'Eduardo', (0, 10, 20, 30, 40)], # 2 
+    ['Luiz', 'João', 'Eduardo', ], # 2 
 ]
 
 # print(salas[2][3][2])
 
 for sala in salas:
-    print(sala)
+    print(f'A sala é {sala}')
+    for aluno in sala:
+        print(aluno)
