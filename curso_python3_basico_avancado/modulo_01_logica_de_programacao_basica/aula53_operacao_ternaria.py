3c5b92226cb79c990a5a531648e65b46d97a8cf6
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-05-12 16:31:26 -0300

    (aula53) operação ternária uma linha só

---
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula53_operacao_ternaria.py
@@ -0,0 +1,6 @@
+"""
+Operação ternária (condicional de uma linha)
+<valor> if <condição> else <outro valor>
+"""
+print('Valor' if True else 'outro valor')
+print('Valor' if False else 'outro valor')
