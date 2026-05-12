c909fb71c4cf13c81981a81c6bf89aaa0eff84e6
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-05-12 16:27:45 -0300

    (desempacotamento) desempacotando na chamada de função

--- curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula52_desempacotamento.py
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula52_desempacotamento.py
@@ -4,6 +4,15 @@ string = 'ABCD'
 lista = ['Maria', 'Helena', 1, 2, 3, 'Eduarda']
 tupla = 'Python', 'e', 'legal'
 
+salas = [
+    # 0        1 
+    ['Maria', 'Helena', ], # 0
+    # 0
+    ['Elaine', ], # 1
+    # 0       1       2
+    ['Luiz', 'João', 'Eduardo', ], # 2 
+]
+
 # Desempacotamento
 # a, b, *_, c = lista
 # print(a, c)
@@ -15,4 +24,9 @@ print()
 
 # Obtendo o mesmo resultado que for
 # desempacotandoa lista
+print('Maria', 'Helena', 1, 2, 3, 'Eduarda') # A mesma ideia do desempacotamento
 print(*lista)
+print(*string)
+print(*tupla)
+
+print(*salas, sep='\n')
