a416f599f5d06995f3f3092eaf8cebd3d3a8512b
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-05-12 16:36:10 -0300

    (aula53) explicando de forma simples operação ternária

--- curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula53_operacao_ternaria.py
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula53_operacao_ternaria.py
@@ -2,5 +2,11 @@
 Operação ternária (condicional de uma linha)
 <valor> if <condição> else <outro valor>
 """
-print('Valor' if True else 'outro valor')
-print('Valor' if False else 'outro valor')
+
+# print('Valor' if True else 'outro valor')
+# print('Valor' if False else 'outro valor')
+
+# Explicando de modo mais facil para entender operação ternária
+condicao = 20 == 21
+variavel = 'valor do if' if condicao else 'valor do else'
+print(f'A condiçao é: {condicao} o resultado é: {variavel}')
