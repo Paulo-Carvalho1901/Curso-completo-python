b7630ae115c7bd1d19d36e21424a88931759aaa2
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-05-12 16:51:52 -0300

    (operacao ternaria) explicando com exemplo do validando digito de duas formas diferentes

--- curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula53_operacao_ternaria.py
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula53_operacao_ternaria.py
@@ -12,5 +12,15 @@ Operação ternária (condicional de uma linha)
 # print(f'A condiçao é: {condicao} o resultado é: {variavel}')
 
 digito = 9 # condição > 9 = 0
+
+# movo digito recebe o valor do digito
+# que novo caso é 9, se a condição do valor do digito for <= 9
+# caso contrario o novo digito recebe 0
 novo_digito = digito if digito <= 9 else 0
+
+# novo digito 2 é = 0, caso a condição do valor do digito
+# seja > 9 caso contrario o valor do digito recebe o valor dele mesmo que no
+# caso é 9
+novo_digito_2 = 0 if digito > 9 else digito
 print(novo_digito)
+print(novo_digito_2)
