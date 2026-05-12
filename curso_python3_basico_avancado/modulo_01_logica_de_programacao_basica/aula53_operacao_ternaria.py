4830086b698914e5ad61cc05be4bf820d625a111
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-05-12 16:43:22 -0300

    (operacao ternaria) validando digito

--- curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula53_operacao_ternaria.py
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula53_operacao_ternaria.py
@@ -7,6 +7,10 @@ Operação ternária (condicional de uma linha)
 # print('Valor' if False else 'outro valor')
 
 # Explicando de modo mais facil para entender operação ternária
-condicao = 20 == 21
-variavel = 'valor do if' if condicao else 'valor do else'
-print(f'A condiçao é: {condicao} o resultado é: {variavel}')
+# condicao = 20 == 21
+# variavel = 'valor do if' if condicao else 'valor do else'
+# print(f'A condiçao é: {condicao} o resultado é: {variavel}')
+
+digito = 9 # condição > 9 = 0
+novo_digito = digito if digito <= 9 else 0
+print(novo_digito)
