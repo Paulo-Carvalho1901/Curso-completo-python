32e34fa54493bb309c619b70cfc6149367d64473
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-05-16 01:39:54 -0300

    (função) definição de funçao

---
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula55_funcoes.py
@@ -0,0 +1,18 @@
+"""
+Introdução às funções (def) em Python
+Funções são trechos de códigos usados para
+replicar determinada ação ao longo do seu código.
+Eles podem receber valores específicos.
+Por padrão, funções Python retornam None (nada).
+"""
+
+# Criando uma função
+def Print():
+    print('Várias1')
+    print('Várias2')
+    print('Várias3')
+    print('Várias4')
+
+Print()
+Print()
+Print()
