02b404cc767c7993125b1564a8d325492a9dd806
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-05-16 02:05:18 -0300

    (função) soma dois valores na funçao

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula56_argumento_nomeado_posicional.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula56_argumento_nomeado_posicional.py
@@ -3,3 +3,8 @@ Argumento nomeado e não nomeado em função Python
 Argumento nomeado tem nome com sinal de igual
 Argumento não nomeado recebe apenas o argumento (valor)
 """
+
+def soma(x, y):
+    print(x + y)
+
+soma(10, 5)
