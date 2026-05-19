0e82199ff8d20ca9440e8ad86d966a211ac07295
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-05-18 23:05:10 -0300

    (Doc) comentarios para explicar parametros e argumentos

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula56_argumento_nomeado_posicional.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula56_argumento_nomeado_posicional.py
@@ -5,8 +5,9 @@ Argumento não nomeado recebe apenas o argumento (valor)
 """
 
 # Argumentos posicionais
-def soma(x, y):
+def soma(x, y): # parametros (x, y)
     print(f'{x=} y={y}', '|', 'x + y =', x + y)
 
-soma(1, 3)
-soma(2, 1)
+
+soma(1, 3) # argumento posicional (é passado de acordo com posição)
+soma(y=2, x=1) # argumentos nomeados (é passo pelo nome dos paramatros no argumento)
