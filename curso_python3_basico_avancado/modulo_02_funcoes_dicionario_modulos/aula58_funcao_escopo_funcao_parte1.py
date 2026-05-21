e9741a0fa1d5ebde0cbe4776b77ce487dde9f275
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-05-21 17:59:12 -0300

    (função) definindo outra função e entendendo sobre como funciona o escopo de função

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula58_funcao_escopo_funcao_parte1.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula58_funcao_escopo_funcao_parte1.py
@@ -7,11 +7,23 @@ O escopo local é o escopo onde apenas nomes do mesmo local
 podem ser alcançados.
 """
 
-x = 10
+x = 1
 
 
 def escopo():
+    global x
+    x = 10
+
+    def outra_funcao():
+        global x
+        x = 11
+        y = 2
+        print(x, y)
+
+    outra_funcao()
     print(x)
 
 
+print(x)
 escopo()
+print(x)
