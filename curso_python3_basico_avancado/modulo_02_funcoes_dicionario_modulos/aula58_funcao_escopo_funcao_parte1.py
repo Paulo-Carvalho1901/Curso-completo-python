82482b8c1026f969c9e81ff194968ea5134f6118
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-05-21 18:01:36 -0300

    (Doc) adicionando comentario para explicação

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula58_funcao_escopo_funcao_parte1.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula58_funcao_escopo_funcao_parte1.py
@@ -5,6 +5,9 @@ Existe o escopo global e local.
 O escopo global é o escopo onde todo o código é alcançavel.
 O escopo local é o escopo onde apenas nomes do mesmo local
 podem ser alcançados.
+Não temos acesso a nomes de escopos internos nos escopos externos.
+A palavra global faz uma variavel do escopo externo ser a mesmo no escopo 
+interno.
 """
 
 x = 1
