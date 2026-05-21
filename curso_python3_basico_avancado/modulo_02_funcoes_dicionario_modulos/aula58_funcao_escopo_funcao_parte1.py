437d179657b5a3d394f054479dc6fbd8b74fdd70
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-05-21 17:17:10 -0300

    (função) definindo um escopo função

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula58_funcao_escopo_funcao_parte1.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula58_funcao_escopo_funcao_parte1.py
@@ -6,3 +6,10 @@ O escopo global é o escopo onde todo o código é alcançavel.
 O escopo local é o escopo onde apenas nomes do mesmo local
 podem ser alcançados.
 """
+
+def escopo():
+    x = 1
+    print(x)
+
+
+escopo()
