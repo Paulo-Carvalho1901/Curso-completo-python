96bd23361a70ec878273b8243294088e7fd80986
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-05-22 00:08:37 -0300

    (função) retono de função com condicional

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula60_funcao_retorno_da_funcao.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula60_funcao_retorno_da_funcao.py
@@ -1,11 +1,12 @@
 # Retorno de valores das funções (return)
 
 def soma(x, y):
-    ...
+    if x > 10:
+        return 55, 33
+    return x + y
 
 
-variavel = soma(1, 2)
-# nome = print('Luiz')
-# nome = None
-variavel = int('1')
-print(variavel)
+soma1 = soma(2, 2)
+soma2 = soma(3, 3)
+# soma3 = soma(10, 2)
+print(soma(9, 2))
