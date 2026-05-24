cbf81934ec82988eada01e1361f3a6a3c8147a26
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-05-23 23:43:57 -0300

    (ex) primeiro exericicio funçao de multiplicando todos os argumentos recebidos

--- curso_python3_basico_avancado/exercicios/ex15.py
+++ curso_python3_basico_avancado/exercicios/ex15.py
@@ -5,6 +5,27 @@
 # Retorne o total para uma variável e mostre o valor
 # da variável
 
+"""
+explicação
+função recebe *args nome da funçao é multiplica
+crio uma variável com total é 1 porque se fosse em zero
+teria um problema pq todo numero * zero é zero.
+depois faço um for em args e multiplico todos eles
+e retorno o total acumulado dessa nultiplicação
+"""
+
+def multiplica(*args): 
+    total = 1
+    for numeros in args:
+        total *= numeros
+    return total
+
+
+multiplica_1 = multiplica(2, 10, 2)
+multiplica_2 = multiplica(1024, 10)
+print(multiplica_1)
+print(multiplica_2)
+
 # Crie uma função fala se um número é par ou impar
 # Retorne se o número é par ou impar
 
