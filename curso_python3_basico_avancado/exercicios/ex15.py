59dce3b1b792d061bfdf491effc157d6317333ed
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-05-23 23:25:43 -0300

    (ex) criado exercicios 15 teste de conhecimento funções

---
+++ curso_python3_basico_avancado/exercicios/ex15.py
@@ -0,0 +1,10 @@
+# Exercícios com funções
+
+# Crie um função que multiplica todos os argumentos
+# não nomeados recebidos
+# Retorne o total para uma variável e mostre o valor
+# da variável
+
+# Crie uma função fala se um número é par ou impar
+# Retorne se o número é par ou impar
+
