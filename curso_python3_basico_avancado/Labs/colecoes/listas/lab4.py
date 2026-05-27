1b00d114b646a80269ea612860b9404c33acbc95
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-05-27 03:37:13 -0300

    (lab) criando inserindo valor em uma lista

---
+++ curso_python3_basico_avancado/Labs/colecoes/listas/lab4.py
@@ -0,0 +1,18 @@
+"""
+(Ex13) da lista de exercicios
+
+Faça uma lista de compras com listas
+o usúario deve ter a a possibilidade de 
+inserir, apagar, e listar valores da sua lista
+não permita que o programa quebre com
+erros de indices inesistentes na lista
+"""
+
+lista_de_compras = []
+
+
+while True:
+    print('Selecione uma opção. ')
+    opcao = input('Digite apenas [I]nserir [a]pagar [l]istar: ')
+
+            
