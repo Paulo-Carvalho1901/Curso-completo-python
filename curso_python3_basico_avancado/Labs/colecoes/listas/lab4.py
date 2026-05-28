fb8d299308d583032e7720f4d7f2ab86cb74454c
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-05-27 23:14:10 -0300

    (LAB) concluido o lab desafio de lista de compras

--- curso_python3_basico_avancado/Labs/colecoes/listas/lab4.py
+++ curso_python3_basico_avancado/Labs/colecoes/listas/lab4.py
@@ -12,7 +12,29 @@ lista_de_compras = []
 
 
 while True:
-    print('Selecione uma opção. ')
+    print('Selecione uma opção: ')
     opcao = input('Digite apenas [I]nserir [a]pagar [l]istar: ')
 
-            
+    if opcao == 'i':
+        valor = input('Valor: ')
+        lista_de_compras.append(valor)
+
+    elif opcao == 'a':
+        indice_str = input(
+            'Escolha uma indice para apagar: '
+        )
+        try:
+            indice = int(indice_str)
+            del lista_de_compras[indice]
+        except ValueError:
+            print('Digitar apenas números inteiros.')
+        except IndexError:
+            print('Indice não existe para ser apagado.')
+    elif opcao == 'l':
+        if len(lista_de_compras) == 0:
+            print('Nada para listar')
+
+        for i, valor in enumerate(lista_de_compras):
+            print(i, valor)
+    else:
+        print('Por favor digite apenas i, a e l.')
