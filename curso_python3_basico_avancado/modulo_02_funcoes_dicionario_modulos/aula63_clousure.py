de4198b38925b1bf8a87e96238a89f83be6bd891
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-05-28 23:50:40 -0300

    (refector) utilizando um for para iterar sobre uma lista de deixando mais dinamico

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula63_clousure.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula63_clousure.py
@@ -9,7 +9,9 @@ def criar_saudacao(saudacao):
 falar_bom_dia = criar_saudacao('Bom dia')
 falar_boa_noite = criar_saudacao('Boa noite')
 
-print(falar_bom_dia('Paulo')) # fazendo o fechamento da função (clousure)
-print(falar_boa_noite('Andreia'))
+# print(falar_bom_dia('Paulo')) # fazendo o fechamento da função (clousure)
+# print(falar_boa_noite('Andreia'))
 
-
+for nome in ['Paulo', 'Andreia', 'Davi']:
+    print(falar_bom_dia(nome))
+    print(falar_boa_noite(nome))
