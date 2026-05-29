d29d395e8c80d930a193f2e620571027276438f6
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-05-28 23:34:25 -0300

    (clousure) executando os paramatros em soudar

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula63_clousure.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula63_clousure.py
@@ -1,10 +1,13 @@
 # Clousure e funções que retornam outras funções
 
 def criar_saudacao(saudacao, nome):
-    return f'{saudacao}, {nome}!'
+    def soudar():
+        return f'{saudacao}, {nome}!'
+    return soudar()
 
 
 saudacao_1 = criar_saudacao('Bom dia', 'Paulo')
 saudacao_2 = criar_saudacao('Boa noite', 'Paulo')
+
 print(saudacao_1)
 print(saudacao_2)
