9b28f1cb47045bd129a24ace570eb9642ad6c769
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-06-08 18:28:41 -0300

    test: escopo e namespace

---
+++ curso_python3_basico_avancado/Labs/Tests/test_escopo.py
@@ -0,0 +1,14 @@
+# Escopo e namespace dos modulos
+
+"""Documentação com docstring"""
+
+um_nome = 'um_nome (GLOBAL)'
+
+
+
+print('Nome do modulo:', __name__)
+print('Arquivo do modulo:', __file__)
+print('Documentação do modulo:', __doc__)
+
+
+print('Fora da função:', um_nome)
