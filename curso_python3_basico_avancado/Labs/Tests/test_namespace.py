1f11e751208fd3381e610e09a9fbd0c447588765
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-06-08 19:12:33 -0300

    Entendendo namespace

---
+++ curso_python3_basico_avancado/Labs/Tests/test_namespace.py
@@ -0,0 +1,11 @@
+# O que é namespace?
+#
+# Namespace é uma estrutura de dados real que mapeia nomes para objetos. Cada
+# chave é o nome que você define e o valor é o objeto correspondente no seu
+# código. Sempre que você cria um nome, essa associação é guardada dentro de um
+# namespace.
+
+
+namespace_global = globals()
+um_nome = "um_nome (GLOBAL)"
+
