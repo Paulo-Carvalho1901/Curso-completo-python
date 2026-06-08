227ddfb06e245107ff52428faea2c022b2eb60f0
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-06-08 18:58:40 -0300

    criado um função global

--- curso_python3_basico_avancado/Labs/Tests/test_escopo.py
+++ curso_python3_basico_avancado/Labs/Tests/test_escopo.py
@@ -2,13 +2,26 @@
 
 """Documentação com docstring"""
 
+
+# nome definido no escopo global (modulo)
 um_nome = 'um_nome (GLOBAL)'
 
 
+# nome definido no escopo global (modulo)
+def func_global(sou_local: str) -> None:
+    # Escopo local (funções e seus parametros)
+
+    # um_nome no escopo local é OUTRA VARIÁVEL (sem relação outro escopo)
+    um_nome = "um_nome (LOCAL)" # nome definido no escopo local
+    outro_nome = "outro_nome (LOCAL)" # nome definido no escopo local
+
+    # Parâmetros de funções também são do escopo local da função
+    print(f"Dentro da função: {um_nome}, {outro_nome}, {sou_local}")
 
 print('Nome do modulo:', __name__)
 print('Arquivo do modulo:', __file__)
 print('Documentação do modulo:', __doc__)
 print()
 
-print('Fora da função:', um_nome)
+
+print('Fora da função:', um_nome) # Escopo GLOBAL
