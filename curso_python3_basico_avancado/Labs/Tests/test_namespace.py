3ea2d5fa4a7644602e9a7821a7992bcbc0669943
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-06-08 19:18:42 -0300

    criado apenas um func_global e seu escopo interno sem execução

--- curso_python3_basico_avancado/Labs/Tests/test_namespace.py
+++ curso_python3_basico_avancado/Labs/Tests/test_namespace.py
@@ -9,3 +9,18 @@
 namespace_global = globals()
 um_nome = "um_nome (GLOBAL)"
 
+
+def func_global(sou_local: str) -> None:
+    um_nome = "um_nome (LOCAL)"
+    outro_nome = "outro_nome (local)"
+    print("LOCAL (namespace da função)")
+    print(locals())
+    print()
+
+
+# func_global("arg (LOCAL)")
+# print()
+
+
+# print("GLOBALS (namespace do modulo)")
+# print(globals())
