9c32bbc187d4b26d0fba57ce41ea1964454a3d8c
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-06-09 00:43:34 -0300

    solução do exercicio forma mais simples possivel

--- curso_python3_basico_avancado/exercicios/ex16.py
+++ curso_python3_basico_avancado/exercicios/ex16.py
@@ -2,3 +2,18 @@
 # Crie uma função que duplique, triplique e quadruplique
 # o número recebido como parâmetro
 
+def duplicar(numero):
+    return numero * 2
+
+
+def triplicar(numero):
+    return numero * 3
+
+
+def quadru(numero):
+    return numero * 4
+
+
+print(duplicar(2))
+print(triplicar(2))
+print(quadru(2))
