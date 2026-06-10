9439e2ac011df9382f6c6b309c7c3a1ce9fb424a
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-06-09 22:31:00 -0300

    dict: basico de manipulação de dicionario pelas chaves

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula65_dict_manipulando_chave.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula65_dict_manipulando_chave.py
@@ -15,5 +15,13 @@ pessoa[chave] = 'Andreia'
 
 # Apagando uma chave
 del pessoa['Sobrenome']
-
 print(pessoa)
+print(pessoa['nome'])
+
+# print(pessoa.get('sobrenome'))
+if pessoa.get('sobrenome') is None:
+    print('NÃO EXISTE')
+else:
+    print(pessoa['Sobrenome'])
+
+# print('ISSO TESTE!')
