6441d4beb60406dfea357e6b0222facab04d226a
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-06-09 22:09:29 -0300

    Manipulando chaves e valores em dicionários

---
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula65_dict_manipulando_chave.py
@@ -0,0 +1,12 @@
+# Manipulando chaves e valores em dicionários
+pessoa = {
+    'nome': 'Luiz Otávio',
+    'sobrenome': 'Miranda',
+    'idade': 18,
+    'altura': 1.8,
+    'endereços': [
+        {'rua': 'tal tal', 'número': 123},
+        {'rua': 'outra rua', 'número': 321},
+    ],
+}
+
