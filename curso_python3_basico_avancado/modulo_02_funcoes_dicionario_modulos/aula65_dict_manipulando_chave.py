6a3f8498eeb0c921e553ddd84bea90c57ce597f8
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-06-09 22:12:22 -0300

    dict: adicionando nome e sobrenome no dicionario separadamente

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula65_dict_manipulando_chave.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula65_dict_manipulando_chave.py
@@ -1,12 +1,10 @@
 # Manipulando chaves e valores em dicionários
-pessoa = {
-    'nome': 'Luiz Otávio',
-    'sobrenome': 'Miranda',
-    'idade': 18,
-    'altura': 1.8,
-    'endereços': [
-        {'rua': 'tal tal', 'número': 123},
-        {'rua': 'outra rua', 'número': 321},
-    ],
-}
+pessoa = {}
 
+##
+##
+
+# Adicionando um item no dicionário
+pessoa['nome'] = 'Paulo Roberto'
+pessoa['Sobrenome'] = 'Carvalho'
+print(pessoa)
