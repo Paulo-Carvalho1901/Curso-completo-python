ea4ac1b529944170c058a37f1ee0cd9542e6cf54
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-01 18:19:51 -0300

    dict: setdefault valores padrês para dict

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula66_dict_metodos_uteis.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula66_dict_metodos_uteis.py
@@ -12,12 +12,15 @@
 pessoa = {
     'nome': 'Luiz Otávio',
     'sobrenome': 'Miranda',
+    # 'idade': 45,
 }
 
 # print(len(pessoa)) # Len retorna quantidade de chaves de um dicionario
 # print(pessoa.keys()) # pegando apenas as chaves
 # print(pessoa.values()) # pega apenas o valor do dict
 # print(pessoa.items()) # pega chave e valor do dict
+pessoa.setdefault('idade', 35)
+print(pessoa['idade'])
 
 for chave, valor in pessoa.items():
     print(chave, valor)
