4d4eb378a84c3a3cfa02ef6f33aa9eea29275f0d
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-03 11:38:45 -0300

    dict: criado copia rasa de lista e de dict

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula67_dict_shallow_copy_deep_copy.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula67_dict_shallow_copy_deep_copy.py
@@ -10,8 +10,25 @@
 # popitem - Apaga o último item adicionado
 # update - Atualiza um dicionário com outro
 
-pessoa = {
-    'nome': 'Paulo',
-    'sobrenome': 'Carvalho',
-    'idade': 37,
+# Exemplo com shallow copy dicionario
+print('Dict')
+d1 = {
+    'c1': 1,
+    'c2': 2,
 }
+
+d2 = d1
+d2['c1'] = 1000
+print('d1', d1)
+print('d2', d2)
+
+print()
+print('Lista')
+# Exemplo com lista shallow copy (copia rasa)
+lista1 = [1, 2, 3, 4]
+lista2 = lista1
+
+lista2[0] = 10 # tambem afeta a lista1, pois são o mesmo valor na memoria
+
+print('lista1', lista1, 'id', id(lista1))
+print('lista2', lista2, 'id', id(lista2))
