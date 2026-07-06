a73e795c39dde95c949373846aeca32a4fe6853e
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-06 10:55:14 -0300

    set: criando um novo set

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula69_set.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula69_set.py
@@ -50,3 +50,4 @@ print(s1)
 # intersecção & (intersection) - Itens presentes em ambos
 # diferença - Itens presentes apenas no set da esquerda
 # diferença simétrica ^ - Itens que não estão em ambos
+s2 = {1, 2, 3}
