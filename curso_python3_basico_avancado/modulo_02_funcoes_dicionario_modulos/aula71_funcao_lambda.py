fb2918d3ea226436c50821af63dd218ce82d5712
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-13 17:53:10 -0300

    list: ordenando uma lista de dicionario

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula71_funcao_lambda.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula71_funcao_lambda.py
@@ -4,21 +4,23 @@
 # que contém apenas uma linha. Ou seja, tudo
 # deve ser contido dentro de uma única
 # expressão.
-# lista = [
-#     {'nome': 'Luiz', 'sobrenome': 'miranda'},
-#     {'nome': 'Maria', 'sobrenome': 'Oliveira'},
-#     {'nome': 'Daniel', 'sobrenome': 'Silva'},
-#     {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
-#     {'nome': 'Aline', 'sobrenome': 'Souza'},
-# ]
+lista = [
+    {'nome': 'Luiz', 'sobrenome': 'miranda'},
+    {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+    {'nome': 'Daniel', 'sobrenome': 'Silva'},
+    {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+    {'nome': 'Aline', 'sobrenome': 'Souza'},
+]
 
 # set_lista = {100, 10, 10, 1, 2, 2, 3, 65}
 # lista_set = list(set_lista)
 # lista_set.sort()
 # print('transformando em lista', lista_set)
 
-lista = [12, 2, 2, 4, 5, 1, 0]
+# lista = [12, 2, 2, 4, 5, 1, 0]
 
-lista.sort() # ordenando
-lista.sort(reverse=True) # mudando a ordem
+# lista.sort() # ordenando
+# lista.sort(reverse=True) # mudando a ordem
+# print(lista)
+
 print(lista)
