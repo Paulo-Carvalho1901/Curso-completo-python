f8744916f244c76ed1a8af07f31824168cfb3bc2
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-14 09:35:46 -0300

    labs: teste de algumas operações em bool

---
+++ curso_python3_basico_avancado/Labs/boolean/labs_bool.py
@@ -0,0 +1,11 @@
+# Entendendo um pouco mais sobre boolean
+
+# True == 1
+# False == 0
+
+print(100 * False)
+print(100 * True)
+
+print()
+print(isinstance(True, bool))
+print(isinstance(True, int))
