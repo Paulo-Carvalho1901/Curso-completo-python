5482428a7c0891490e1e09b01fa9802d35224fc1
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-16 21:28:11 -0300

    func: forma mais otimizada de ordenar uma lista com lambda

--- curso_python3_basico_avancado/Labs/lambda/labs_lambda_01.py
+++ curso_python3_basico_avancado/Labs/lambda/labs_lambda_01.py
@@ -15,8 +15,26 @@ cadastro_pessoa = [
 # for item in cadastro_pessoa:
 #     print(item)
 
+##############################################################################
 # Outra forma de ordenar mais agora com função lambda
-cadastro_pessoa.sort(key=lambda item: item['nome'])
+# cadastro_pessoa.sort(key=lambda item: item['nome'])
 
-for item in cadastro_pessoa:
-    print(item)
+# for item in cadastro_pessoa:
+#     print(item)
+
+###############################################################################
+# Outra forma mais simplificada de lambda
+
+def exibir(lista):
+    for item in lista:
+        print(item)
+    print()
+    
+
+l1 = sorted(cadastro_pessoa, key=lambda item: item['nome'])
+l2 = sorted(cadastro_pessoa, key=lambda item: item['sobrenome'])
+l3 = sorted(cadastro_pessoa, key=lambda item: item['idade'])
+
+exibir(l1)
+exibir(l2)
+exibir(l3)
