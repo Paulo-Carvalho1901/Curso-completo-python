dad626c688cdf5048ad748c01c99110f96d05b88
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-17 13:50:02 -0300

    aula72: funcao lambda de multiplicador

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula72_funcao_lambda_complexa.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula72_funcao_lambda_complexa.py
@@ -4,21 +4,24 @@ def executa(funcao, *args):
     return funcao(*args)
 
 
-def soma(x, y):
-    return x + y
+# def soma(x, y):
+#     return x + y
 
 
-def criar_multiplicador(multiplicador):
-    def multiplica(numero):
-        return numero * multiplicador
-    return multiplica
+# def criar_multiplicador(multiplicador):
+#     def multiplica(numero):
+#         return numero * multiplicador
+#     return multiplica
 
+duplica = executa(
+    lambda m: lambda n: n * m,
+    2
+)
+print(duplica(2))
 
 print(
     executa(
         lambda x, y: x + y,
         2, 3
     ),
-    executa(soma, 2, 3),
-    soma(2, 3)
 )
