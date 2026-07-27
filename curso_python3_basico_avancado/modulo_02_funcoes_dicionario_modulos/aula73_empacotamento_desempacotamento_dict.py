b2027d518b3ffab177eb3726e87dc4f14aaf8da9
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-27 18:01:51 -0300

    dict: unindo dois dict diferentes

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula73_empacotamento_desempacotamento_dict.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula73_empacotamento_desempacotamento_dict.py
@@ -4,15 +4,29 @@ a, b = 1, 2
 a, b = b, a
 # print(a, b)
 
+# pessoa = {
+#     'nome': 'Paulo',
+#     'sobrenome': 'Carvalho'
+# }
+
+# (a1, a2), (b1, b2) = pessoa.items()
+# print(a1, a2)
+# print(b1, b2)
+# print()
+
+# for chave, valor in pessoa.items():
+#     print(chave, valor)
+
 pessoa = {
     'nome': 'Paulo',
     'sobrenome': 'Carvalho'
 }
 
-(a1, a2), (b1, b2) = pessoa.items()
-print(a1, a2)
-print(b1, b2)
-print()
+dados_pessoas = {
+    'idade': 16,
+    'altura': 1.6,
+}
 
-for chave, valor in pessoa.items():
-    print(chave, valor)
+pessoa_completa = {**pessoa, **dados_pessoas}
+
+print(pessoa_completa)
