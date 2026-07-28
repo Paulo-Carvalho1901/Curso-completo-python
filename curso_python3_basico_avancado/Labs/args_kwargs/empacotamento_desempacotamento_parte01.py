acfa7960ec4a7971c751813528cd41d4817564e9
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-28 16:57:35 -0300

    dict: recebendo apenas as chaves do dict

--- curso_python3_basico_avancado/Labs/args_kwargs/empacotamento_desempacotamento_parte01.py
+++ curso_python3_basico_avancado/Labs/args_kwargs/empacotamento_desempacotamento_parte01.py
@@ -3,3 +3,11 @@
 a, b = 1, 2 # empacotamento
 a, b = b, a # invertendo os valores
 # print(a, b)
+
+cadastro = {
+    'nome': 'Paulo',
+    'sobrenome': 'Carvalho',
+}
+
+a, b = cadastro
+print(a, b)
