185839ba989c7962be8a91d5daf2fab14403176c
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-28 18:20:13 -0300

    doc: dando exemplos de kwargs

---
+++ curso_python3_basico_avancado/Labs/args_kwargs/args_kwargs.py
@@ -0,0 +1,15 @@
+# args - argumentos não nomeados
+# kwargs - argumentos nomeados
+
+# * trabalha com uma sequência de valores (tuple, lista, etc.)
+# ** trabalha com pares chave-valor (dict)
+
+# EMPACOTANDO EM UM DICT
+# def empacote(**kwargs):
+#     print(kwargs)
+
+# pessoa = empacote(nome='Paulo', sobrenome='Roberto', idade=17)
+
+
+########################################################################
+
