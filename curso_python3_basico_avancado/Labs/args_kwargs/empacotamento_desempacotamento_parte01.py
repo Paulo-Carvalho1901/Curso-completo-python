cd7ad12833969e46c71f4016420e7ad2d6776c07
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-28 17:56:24 -0300

    dict: unindo dois dict en 1

--- curso_python3_basico_avancado/Labs/args_kwargs/empacotamento_desempacotamento_parte01.py
+++ curso_python3_basico_avancado/Labs/args_kwargs/empacotamento_desempacotamento_parte01.py
@@ -4,10 +4,10 @@ a, b = 1, 2 # empacotamento
 a, b = b, a # invertendo os valores
 # print(a, b)
 
-cadastro = {
-    'nome': 'Paulo',
-    'sobrenome': 'Carvalho',
-}
+# cadastro = {
+#     'nome': 'Paulo',
+#     'sobrenome': 'Carvalho',
+# }
 
 # UMA FORMA DE SE DESEMPACOTAR
 # a metodos items() retorna chave e valor do dict
@@ -18,5 +18,14 @@ cadastro = {
 #########################################################################
 
 # OUTRA FORMA DE SE DESEMPACOTAR
-for chave, valor in cadastro.items():
-    print(chave, valor)
+# for chave, valor in cadastro.items():
+#     print(chave, valor)
+
+##########################################################################
+
+# MAIS UMA MANEIRA DE SE PENSAR EM EMPACOTAMENTO E DESEMPACOTAMENTO
+
+cadastro = {
+    'nome': 'Paulo',
+    'sobrenome': 'Carvalho',
+}
