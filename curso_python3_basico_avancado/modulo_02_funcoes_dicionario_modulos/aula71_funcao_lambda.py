b3039dbff29498194365b634f2473c5e508487c7
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-29 16:15:39 -0300

    doc: add comentarios para melhor entender o codigo

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula71_funcao_lambda.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula71_funcao_lambda.py
@@ -4,6 +4,10 @@
 # que contém apenas uma linha. Ou seja, tudo
 # deve ser contido dentro de uma única
 # expressão.
+# lista = [32, 2, 5, 55, 4, 8, 10, 0, 1, 2, 3]
+# lista.sort(reverse=True)
+# print(lista)
+
 lista = [
     {'nome': 'Luiz', 'sobrenome': 'miranda'},
     {'nome': 'Maria', 'sobrenome': 'Oliveira'},
@@ -12,26 +16,31 @@ lista = [
     {'nome': 'Aline', 'sobrenome': 'Souza'},
 ]
 
+# Função chave que irar ordenanr dicionario.
+def ordena(item):
+    return item['nome']
 
-# def ordena(item):
-#     return item['nome']
+# Nesta estou utilizando a função ordena
+# para gerar a ordenação do dicionario.
+lista.sort(key=ordena) # fazendo a ordenação
+# print(lista)
 
-# lista.sort(key=ordena) # fazendo a ordenação
-# for item in lista:
-#     print(lista)
+# Organizando para melhor os dados com for.
+for item in lista:
+    print(item)
 
 
 # Expressão lambda
 # no caso o primeiro item é parâmetro e segundo item é retorno da função
 # lista.sort(key=lambda item: item['nome'])
 
-def exibir(lista):
-    for item in lista:
-        print(item)
-    print()
+# def exibir(lista):
+#     for item in lista:
+#         print(item)
+#     print()
 
-l1 = sorted(lista, key=lambda item: item['nome'])
-l2 = sorted(lista, key=lambda item: item['sobrenome'])
+# l1 = sorted(lista, key=lambda item: item['nome'])
+# l2 = sorted(lista, key=lambda item: item['sobrenome'])
 
-exibir(l1)
-exibir(l2)
+# exibir(l1)
+# exibir(l2)
