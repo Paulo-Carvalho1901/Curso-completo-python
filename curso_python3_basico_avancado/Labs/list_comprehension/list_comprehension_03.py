5c856093f7c5f97ff78d7911b99dfc8fbeaa69e2
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-29 17:49:36 -0300

    labs: desconto de 10%

---
+++ curso_python3_basico_avancado/Labs/list_comprehension/list_comprehension_03.py
@@ -0,0 +1,8 @@
+# Exemplo prático
+# Imagine que você tenha uma lista de preços e 
+# queira aplicar um desconto de 10%:
+
+precos = [100, 200, 300, 450]
+
+preco_com_desconto = [preco * 0.9 for preco in precos]
+print(preco_com_desconto)
