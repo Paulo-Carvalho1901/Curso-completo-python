a68455de1bbe420d7d487cbc0d212d8189dbdbe1
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-29 18:14:16 -0300

    mapeamento com list comprehension sobre preco > 20

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula75_list_comprehension_map.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula75_list_comprehension_map.py
@@ -5,7 +5,12 @@ produtos = [
     {'nome': 'p3', 'preco': 30, },
 ]
 
-novos_produtos = [{**produto, 'preco': produto['preco'] * 1.05} for produto in produtos]
+novos_produtos = [
+    # Mapeamento list comprehension
+    {**produto, 'preco': produto['preco'] * 1.05}
+    if produto['preco'] > 20 else {**produto}
+    for produto in produtos
+]
 
 # print(novos_produtos)
 print(*novos_produtos, sep='\n')
