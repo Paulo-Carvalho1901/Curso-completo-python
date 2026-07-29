51e034f1aece4496ab7651a5157aa305e8583097
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-29 16:19:03 -0300

    lambda: outra forma de ordenar

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula71_funcao_lambda.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula71_funcao_lambda.py
@@ -17,23 +17,30 @@ lista = [
 ]
 
 # Função chave que irar ordenanr dicionario.
-def ordena(item):
-    return item['nome']
+# def ordena(item):
+#     return item['nome']
 
 # Nesta estou utilizando a função ordena
 # para gerar a ordenação do dicionario.
-lista.sort(key=ordena) # fazendo a ordenação
+# lista.sort(key=ordena) # fazendo a ordenação
 # print(lista)
 
 # Organizando para melhor os dados com for.
-for item in lista:
-    print(item)
+# for item in lista:
+#     print(item)
 
+###############################################################################
 
 # Expressão lambda
 # no caso o primeiro item é parâmetro e segundo item é retorno da função
 # lista.sort(key=lambda item: item['nome'])
 
+# Organizando para melhor os dados com for
+# for item in lista:
+#     print(item)
+
+
+###############################################################################
 # def exibir(lista):
 #     for item in lista:
 #         print(item)
