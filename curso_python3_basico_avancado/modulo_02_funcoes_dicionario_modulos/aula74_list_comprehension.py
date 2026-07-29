9495af64563d838448e36105e8d196e386ff204d
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-28 23:24:44 -0300

    list: criando uma lista fazia e add numero sobre ela

---
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula74_list_comprehension.py
@@ -0,0 +1,13 @@
+# List comprehension em Python
+# List comprehension é uma forma mais rapida de criar listas
+# a partir de iterável
+# print(list(range(10)))
+
+# Uma solução seria para fazer isso print(list(range(10)))
+
+lista = []
+for numero in range(10):
+    lista.append(numero)
+print(lista)
+
+#######################################################################
