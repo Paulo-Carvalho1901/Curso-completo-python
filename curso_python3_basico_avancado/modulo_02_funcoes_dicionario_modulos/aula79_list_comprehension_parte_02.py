f3c940ee13a0d28389ff166cf873f1bbd1050e0d
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-31 18:05:57 -0300

    list: com list comprehension fazendo map alterando as listas com operação matematica

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula79_list_comprehension_parte_02.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula79_list_comprehension_parte_02.py
@@ -5,9 +5,13 @@ numeros = [1, 2, 3, 4, 5]
 # criado list comprehension
 # novos_numeros = [numero for numero in numeros]
 divisao = [numero / 2 for numero in numeros]
+multiplicacao = [numero * 2 for numero in numeros]
+quadrado = [numero ** 2 for numero in numeros]
 
 # alterando a lista numeros
 numeros[0] = 20
 
-print('numeros', numeros)
-print('novos_numeros', divisao)
+print('lista original', numeros)
+print('divisao', divisao)
+print('multiplicação', multiplicacao)
+print('quadrado', quadrado)
