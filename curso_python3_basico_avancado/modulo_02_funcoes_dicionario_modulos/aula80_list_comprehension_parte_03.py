1880da3b087b637cc086a7313e5c530d708cdd15
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-31 18:34:49 -0300

    list: criado um condicao com if para alterar um valor

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula80_list_comprehension_parte_03.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula80_list_comprehension_parte_03.py
@@ -2,11 +2,20 @@
 
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+# Filtro
 novos_numeros = [numero for numero in numeros if numero > 5]
 impares = [numero for numero in numeros if numero % 2 != 0]
 pares = [numero for numero in numeros if numero % 2 == 0]
+
+# Outro if
+outro_if = [
+    numero if numero != 6 else 600
+    for numero in numeros 
+    if numero % 2 == 0
+]
 
 print(numeros)
 print(novos_numeros)
 print(impares)
 print(pares)
+print(outro_if)
