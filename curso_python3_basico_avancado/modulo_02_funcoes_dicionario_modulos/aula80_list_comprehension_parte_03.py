359b6cfaea3f3b051d991c3e20823eeff6d0621d
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-31 18:25:19 -0300

    list comprehension: criado algumas filtros na lista

---
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula80_list_comprehension_parte_03.py
@@ -0,0 +1,12 @@
+# List comprehension Filter
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+novos_numeros = [numero for numero in numeros if numero > 5]
+impares = [numero for numero in numeros if numero % 2 == 1]
+pares = [numero for numero in numeros if numero % 2 == 0]
+
+print(numeros)
+print(novos_numeros)
+print(impares)
+print(pares)
