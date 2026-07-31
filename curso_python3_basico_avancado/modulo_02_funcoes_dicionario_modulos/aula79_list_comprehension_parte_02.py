13b3f81e12ed29cce1e8529813a8f0e68d1d6a33
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-31 18:01:20 -0300

    list: dividindo cada numero da lista por 2

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula79_list_comprehension_parte_02.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula79_list_comprehension_parte_02.py
@@ -3,10 +3,11 @@
 numeros = [1, 2, 3, 4, 5]
 
 # criado list comprehension
-novos_numeros = [numero for numero in numeros]
+# novos_numeros = [numero for numero in numeros]
+divisao = [numero / 2 for numero in numeros]
 
 # alterando a lista numeros
 numeros[0] = 20
 
 print('numeros', numeros)
-print('novos_numeros', novos_numeros)
+print('novos_numeros', divisao)
