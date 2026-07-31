3d7530f2b75ea382031098cd900c080c51988838
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-31 18:27:10 -0300

    operacao: mais uma forma de achar um numero impar

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula80_list_comprehension_parte_03.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula80_list_comprehension_parte_03.py
@@ -3,7 +3,7 @@
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 novos_numeros = [numero for numero in numeros if numero > 5]
-impares = [numero for numero in numeros if numero % 2 == 1]
+impares = [numero for numero in numeros if numero % 2 != 0]
 pares = [numero for numero in numeros if numero % 2 == 0]
 
 print(numeros)
