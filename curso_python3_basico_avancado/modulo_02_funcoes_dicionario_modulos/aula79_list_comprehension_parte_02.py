6d8ddd34d5a1342e84d7c90c5964783493ff7bf5
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-31 18:12:27 -0300

    list: criado apenas func simples para trabalhar com list comprehension

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula79_list_comprehension_parte_02.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula79_list_comprehension_parte_02.py
@@ -1,12 +1,24 @@
 # List comprehension
 
+def divisaoFn(x, y):
+    return x / y
+
+
+def multiplicacaoFn(x, y):
+    return x * y
+
+
+def potenciacaoFn(x, y):
+    return x ** y
+
+
 numeros = [1, 2, 3, 4, 5]
 
 # criado list comprehension
 # novos_numeros = [numero for numero in numeros]
-divisao = [numero / 2 for numero in numeros]
-multiplicacao = [numero * 2 for numero in numeros]
-quadrado = [numero ** 2 for numero in numeros]
+divisao = [divisaoFn(numero, 2) for numero in numeros]
+multiplicacao = [multiplicacaoFn(numero, 2) for numero in numeros]
+quadrado = [potenciacaoFn(numero, 2) for numero in numeros]
 
 # alterando a lista numeros
 numeros[0] = 20
