5d36973955ba36f9424955999cdbe2c14af96270
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-31 17:59:27 -0300

    list: basico de list comprehension

---
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula79_list_comprehension_parte_02.py
@@ -0,0 +1,12 @@
+# List comprehension
+
+numeros = [1, 2, 3, 4, 5]
+
+# criado list comprehension
+novos_numeros = [numero for numero in numeros]
+
+# alterando a lista numeros
+numeros[0] = 20
+
+print('numeros', numeros)
+print('novos_numeros', novos_numeros)
