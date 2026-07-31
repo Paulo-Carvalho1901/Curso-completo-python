dda751c258c8b5176f11b78a6e38f9fc9b2ba7fe
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-31 17:54:48 -0300

    list: criado uma nova lista com for

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula78_list_comprehension_duvidas.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula78_list_comprehension_duvidas.py
@@ -19,7 +19,12 @@ numeros = [1, 2, 3, 4, 5]
 #
 
 # shallow copy com list comprehension
-novos_numeros = [numero for numero in numeros]
+# novos_numeros = [numero for numero in numeros]
+
+# agora com for criando uma nova lista
+novos_numeros = []
+for numero in numeros:
+    novos_numeros.append(numero)
 
 numeros[0] = 20
 print(novos_numeros)
