055e04f2d8c2c96b96dc34982c2523ee3c56f485
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-31 23:59:00 -0300

    inserindo join unindo as letras com ponto

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula82_list_comprehension_strings.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula82_list_comprehension_strings.py
@@ -4,9 +4,9 @@
 string = 'Paulo Carvalho'
 
 numeros_de_letras = 2
-nova_string = [
+nova_string = '.'.join([
     string[indice:indice + numeros_de_letras]
     for indice in range(0, len(string), numeros_de_letras)
-]
+])
 
 print(nova_string)
