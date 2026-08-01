952b990fa3465c4a8e5740f57dce079b6a7c2a0a
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-01 00:07:14 -0300

    deixando a ultima letra maiuscula do nome

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula82_list_comprehension_strings.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula82_list_comprehension_strings.py
@@ -9,4 +9,14 @@ nova_string = '.'.join([
     for indice in range(0, len(string), numeros_de_letras)
 ])
 
-print(nova_string)
+# print(nova_string)
+
+##########################################################################
+
+nomes = ['paulo', 'andreia', 'davi', 'joão', 'felipe']
+novos_nomes = [
+    f'{nome[:-1].lower()}{nome[-1].upper()}'
+    for nome in nomes
+]
+
+print(novos_nomes)
