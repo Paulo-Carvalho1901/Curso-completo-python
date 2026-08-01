30771c61c4d95172ce21bfeff5234cd021525e72
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-07-31 23:56:59 -0300

    list comprehension: trabalhando strings

---
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula82_list_comprehension_strings.py
@@ -0,0 +1,12 @@
+# list comprehension
+# trabalhando com strings
+
+string = 'Paulo Carvalho'
+
+numeros_de_letras = 2
+nova_string = [
+    string[indice:indice + numeros_de_letras]
+    for indice in range(0, len(string), numeros_de_letras)
+]
+
+print(nova_string)
