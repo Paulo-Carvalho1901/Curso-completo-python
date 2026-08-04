842b50f1b6491297cec663568a869afded9d63a4
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-04 20:51:07 -0300

    iterator: utilizando a função iter

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula87_Iterables_Iterators.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula87_Iterables_Iterators.py
@@ -1,4 +1,5 @@
 # Generator expression, iterables e iterators em Pthon
 
 iterables = ['Eu', 'Tenho', '__iter__']
-iterator = iterables.__iter__() # tem __iter__ e __next__
+#iterator = iterables.__iter__() # tem __iter__ e __next__
+iterator = iter(iterables)
