1970c67f1ecc4bf1213ce894d70dacc6aceb2d0b
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-04 18:36:37 -0300

    hasttr: verifica se determinado metodos existe

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula86_dir_hasattr_getattr.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula86_dir_hasattr_getattr.py
@@ -2,5 +2,11 @@
 
 string = 'Paulo'
 
-print(string)
-print(dir(string)) # tras todos os metosos disponiveis para uso
+# print(string)
+# print(dir(string)) # tras todos os metosos disponiveis para uso
+
+##############################################################################
+
+if hasattr(string, 'upper'):
+    print('Existe upper')
+    print(string.upper())
