02882a67eb19e5f4ceddafcafa311d501f7f1db6
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-04 20:59:51 -0300

    generator: função que sabe pausar

---
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula88_generator.py
@@ -0,0 +1,6 @@
+# Generator expression, Iterables and Iterator in Python
+# Basicamente são funções que sabem quando pausar.
+
+iterable = ['I', 'Have', '__iter__']
+iterator = iter(iterable) # Have __iter__ and __next__
+
