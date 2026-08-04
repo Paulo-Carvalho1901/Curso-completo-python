e9100a71c70575df73b2c6611986a241e0834bf2
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-04 14:39:25 -0300

    refector: corrigindo a forma dos dados da lista que esta sendo testada

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula84_isinstace.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula84_isinstace.py
@@ -1,8 +1,7 @@
-# isistance - para obter se objeto é de determinado tipo
-
+# isinstace - para saber se objeto é de determinado tipo
 lista = [
-    'a', 1, 1, 1, True, [0, 1, 2], (1, 2),
-    {0, 1}, {'nome', 'Luiz'},
+    'a', 1, 1.1, True, [0, 1, 2], (1, 2),
+    {0, 1}, {'nome': 'Luiz'},
 ]
 
 for item in lista:
