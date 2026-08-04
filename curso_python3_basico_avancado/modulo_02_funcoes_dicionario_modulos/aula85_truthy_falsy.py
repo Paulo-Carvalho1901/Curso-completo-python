a7ad0756368911c4d6a60ff782de91b575da5bfd
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-04 16:10:38 -0300

    valores falsos e verdadeiros em Python

---
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula85_truthy_falsy.py
@@ -0,0 +1,15 @@
+# Valor Truthy e Falsy, tipos mutáveis e imutáveis
+# Mutáveis [] {} set()
+# Imutáveis () "" 0 0.0 None False range(0, 10)
+
+lista = []
+dicionario = {}
+conjunto = set()
+tupla = ()
+string = ''
+inteiro = 0
+flutuante = 0.0
+nada = None
+falso = False
+intervalo = range(0)
+
