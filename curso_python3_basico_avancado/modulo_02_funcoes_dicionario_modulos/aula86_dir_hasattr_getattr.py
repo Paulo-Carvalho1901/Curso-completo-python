a30295b23e80bc01e1c4c0ef6648111bc8b55391
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-04 18:43:00 -0300

    getattr: pegando o metodo de dentro da string

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula86_dir_hasattr_getattr.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula86_dir_hasattr_getattr.py
@@ -1,12 +1,22 @@
 # dir, hasattr e getattr em Python
-
+# dir
 string = 'Paulo'
 
 # print(string)
 # print(dir(string)) # tras todos os metosos disponiveis para uso
 
 ##############################################################################
-
+# hasttr
 if hasattr(string, 'upper'):
     print('Existe upper')
     print(string.upper())
+    print()
+
+##############################################################################
+# getattr
+metodo = 'upper1'
+if hasattr(string, metodo):
+    print('Existe upper')
+    print(getattr(string, metodo)())
+else:
+    print('Não existe o metodo', metodo)
