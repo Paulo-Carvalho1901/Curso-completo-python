43fed24a31247bde6d65fa6829cb1fbad65933bc
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-04 14:32:26 -0300

    validando se um item da lista é um set e add o valor 5

---
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula84_isinstace.py
@@ -0,0 +1,11 @@
+# isistance - para obter se objeto é de determinado tipo
+
+lista = [
+    'a', 1, 1, 1, True, [0, 1, 2], (1, 2),
+    {0, 1}, {'nome', 'Luiz'},
+]
+
+for item in lista:
+    if isinstance(item, set):
+        item.add(5)
+        print(item, isinstance(item, set))
