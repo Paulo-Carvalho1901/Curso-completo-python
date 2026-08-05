00220cf992f4c6bc05f60852b9e6d1e4c9c6d0d1
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-05 15:57:06 -0300

    lista: verificando o valor da lista em bytes na memoria

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula88_generator.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula88_generator.py
@@ -9,6 +9,12 @@ iterator = iter(iterable) # Have __iter__ and __next__
 # List comprehension
 lista = [numero for numero in range(10)]
 
+# Varificando varios valores na memória
+for n in [10, 100, 1000, 10000]:
+    lista = [i for i in range(n)]
+    print(n, sys.getsizeof(lista))
+print()
+
 # Generator expression - next(generator)
 generator = (numero for numero in range(10))
 
