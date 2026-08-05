341b56a5f251395eafc04f82fb95a2a8db0a4b24
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-05 18:15:24 -0300

    tratando mais de duas essessões no mesmo except

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula91_try_except.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula91_try_except.py
@@ -3,14 +3,16 @@
 try:
     a = 10
     b = 0
-    print(b[0])
-    print('Linha 1')
+    # print(b[0])
+    print('Linha 1'[1000])
     c = a / b
     print('linha 2')
 except ZeroDivisionError:
     print('Dividiu por zero.')
 except NameError:
     print('variável b não está definida.')
+except (TypeError, IndexError):
+    print('TypeError + IndexError')
 except Exception:
     print('ERRO DESCONHECIDO.')
 
