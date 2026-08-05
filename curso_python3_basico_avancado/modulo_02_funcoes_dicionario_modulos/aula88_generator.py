a3fef7eb5b44c41c4fe46a7c4becc70fc0fa2f7f
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-04 21:21:48 -0300

    tamanho em byte do generator

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula88_generator.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula88_generator.py
@@ -1,13 +1,16 @@
 # Generator expression, Iterables and Iterator in Python
 # Basicamente são funções que sabem quando pausar.
 
+import sys
+
 iterable = ['I', 'Have', '__iter__']
 iterator = iter(iterable) # Have __iter__ and __next__
 
 # List comprehension
-# generator = [numero for numero in range(10)]
+lista = [numero for numero in range(10)]
 
 # Generator expression - next(generator)
 generator = (numero for numero in range(10))
 
-print(next(generator))
+print(sys.getsizeof(lista)) # vendo tamaho da lista em byte
+print(sys.getsizeof(generator))
