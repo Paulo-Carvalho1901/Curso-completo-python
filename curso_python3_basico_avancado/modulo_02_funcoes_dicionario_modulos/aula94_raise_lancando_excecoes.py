02f535c57815301ecfbc11156767ba4eda98eac1
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-13 00:08:57 -0300

    criando funções para separar resposabilidades de tratamento de erro

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula94_raise_lancando_excecoes.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula94_raise_lancando_excecoes.py
@@ -3,11 +3,22 @@
 
 def nao_aceito_zero(d):
     if d == 0:
-        raise ZeroDivisionError('Voce está tendando dividir por zero.')
+        raise ZeroDivisionError ('Voce está tendando dividir por zero.')
+    return True
+
+
+def deve_ser_int_float(n):
+    if not isinstance(n, (float, int)):
+        raise TypeError(
+            f'{n} deve ser int ou float'
+        ) 
+    return True
 
 
 def divide(n, d):
+    deve_ser_int_float(n)
+    deve_ser_int_float(d)
     nao_aceito_zero(d)
     return n / d
 
-print(divide(8, 0))
+print(divide(8, '0'))
