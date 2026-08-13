5ecef4cc8e8d54db7a003fd82f9f00bc321da9c5
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-12 23:55:36 -0300

    simulando erro ma pratica

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula94_raise_lancando_excecoes.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula94_raise_lancando_excecoes.py
@@ -1,6 +1,11 @@
 # raise - lançando exceções (erros)
 # https://docs.python.org/pt-br/3/library/exceptions.html#built-in-exceptions
 
-# print(123)
-# raise ValueError('Deu erro.')
-# print(456)
+def divide(n, d):
+    try:
+        return n / d
+    except ZeroDivisionError:
+        return n
+
+
+print(divide(8, 0))
