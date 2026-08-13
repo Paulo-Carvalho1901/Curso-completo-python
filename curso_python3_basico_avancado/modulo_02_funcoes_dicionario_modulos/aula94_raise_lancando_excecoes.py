7b9a35d3fb9c2df5d7a045996ab8a56addffb236
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-13 00:03:28 -0300

    criando um funcao para tratar erro dividr por zero

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula94_raise_lancando_excecoes.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula94_raise_lancando_excecoes.py
@@ -1,9 +1,13 @@
 # raise - lançando exceções (erros)
 # https://docs.python.org/pt-br/3/library/exceptions.html#built-in-exceptions
 
-def divide(n, d):
+def nao_aceito_zero(d):
     if d == 0:
         raise ZeroDivisionError('Voce está tendando dividir por zero.')
+
+
+def divide(n, d):
+    nao_aceito_zero(d)
     return n / d
 
 print(divide(8, 0))
