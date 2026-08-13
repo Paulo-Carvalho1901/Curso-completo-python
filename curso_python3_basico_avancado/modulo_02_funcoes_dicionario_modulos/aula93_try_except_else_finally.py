3263271490a1e8b88cfcb7d51bcc231ccdb77c21
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-12 23:41:45 -0300

    utilizando else tratamento de erro caso não de erro na aplicação

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula93_try_except_else_finally.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula93_try_except_else_finally.py
@@ -2,8 +2,10 @@
 
 try:
    print('Abril o arquivo')
-   8 / 0
+   # 8 / 0
 except ZeroDivisionError:
    print('Dividiu por zero.')
+else:
+   print('Não deu erro...')
 finally: # sempre executado
    print('Fechar aquivo...')
