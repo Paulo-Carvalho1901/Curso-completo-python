08224e010c256764ece8cbc55de7a8c16ba922fd
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-13 21:43:57 -0300

    alterandonome dos modulos para nomes menores com as

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula95_modulos.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula95_modulos.py
@@ -21,18 +21,18 @@
 # print(platform)
 
 # alias 1 - import nome_modulo as apelido
-import sys as s
+# import sys as s
 
-sys = 'alguma coisa'
-print(s.platform)
-print(sys)
+# sys = 'alguma coisa'
+# print(s.platform)
+# print(sys)
 
 
 # alias 2 - from nome_modulo import objeto as apelido
-# from sys import exit as ex
-# from sys import platform as pf
+from sys import exit as ex
+from sys import platform as pf
 
-# print(pf)
+print(pf)
 
 # Vantagens: você pode reservar nomes para seu código
 # Desvantagens: pode ficar fora do padrão da linguagem
