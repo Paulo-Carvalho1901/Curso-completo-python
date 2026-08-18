53b17ec86266b96f580d2b6641a5fcf0d0f98e39
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-17 22:52:49 -0300

    3° forma de importação do modulo

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula100_introducao_packages.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula100_introducao_packages.py
@@ -2,8 +2,10 @@ from sys import path
 
 import aula100_package.modulo
 from aula100_package.modulo import soma_do_modulo
+from aula100_package import modulo
 
 # print(*path, sep='\n')
 
 print(soma_do_modulo(2, 3))
 print(aula100_package.modulo.soma_do_modulo(2, 3))
+print(modulo.soma_do_modulo(2, 3))
