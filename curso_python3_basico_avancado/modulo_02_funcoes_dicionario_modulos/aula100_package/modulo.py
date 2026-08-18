e2229ed736d31b46494d1dc1ca7348c61e18ca51
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-17 23:46:57 -0300

    criado uma funçao fala oi

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula100_package/modulo.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula100_package/modulo.py
@@ -1,8 +1,11 @@
-__all__ = [
-    'variavel',
-    'soma_do_modulo',
-    'nova_variavel',
-]
+# __all__ = [
+#     'variavel',
+#     'soma_do_modulo',
+#     'nova_variavel',
+# ]
+
+
+from modulo_b import fala_oi
 
 variavel = 'Apenas isso do modulo quando usar *'
 
@@ -10,4 +13,6 @@ variavel = 'Apenas isso do modulo quando usar *'
 def soma_do_modulo(x, y):
     return x + y
 
+
 nova_variavel = 'outra coisa'
+fala_oi()
