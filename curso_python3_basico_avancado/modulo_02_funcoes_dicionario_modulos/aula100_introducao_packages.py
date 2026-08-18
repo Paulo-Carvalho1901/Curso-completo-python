4041721ab470ee182a5a3aa8de61bb5ee30ca56c
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-17 22:57:54 -0300

    determinando os acessos do modulo atravez de paramatros dentro do all

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula100_introducao_packages.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula100_introducao_packages.py
@@ -5,10 +5,11 @@ from aula100_package.modulo import soma_do_modulo
 from aula100_package import modulo
 
 # Má pratica de importação
-# from aula100_package.modulo import *
+from aula100_package.modulo import *
 
 # print(*path, sep='\n')
 
 print(soma_do_modulo(2, 3))
 print(aula100_package.modulo.soma_do_modulo(2, 3))
 print(modulo.soma_do_modulo(2, 3))
+print(variavel)
--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula100_package/modulo.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula100_package/modulo.py
@@ -1,3 +1,11 @@
+__all__ = [
+    'variavel',
+    'soma_do_modulo',
+]
+
+variavel = 'Apenas isso do modulo quando usar *'
+
+
 def soma_do_modulo(x, y):
     return x + y
 
