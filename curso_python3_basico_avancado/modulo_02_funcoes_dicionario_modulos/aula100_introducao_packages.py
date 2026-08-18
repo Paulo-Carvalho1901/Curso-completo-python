6fa0d98e39c71d968407892563dcdd16a3b1749e
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-17 22:54:01 -0300

    Má pratca de importação dos modulos

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula100_introducao_packages.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula100_introducao_packages.py
@@ -4,6 +4,9 @@ import aula100_package.modulo
 from aula100_package.modulo import soma_do_modulo
 from aula100_package import modulo
 
+# Má pratica de importação
+# from aula100_package.modulo import *
+
 # print(*path, sep='\n')
 
 print(soma_do_modulo(2, 3))
