c2601cb3bb93b0461a5838b0fd07bb55b3dcd2f8
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-17 23:41:39 -0300

    adicionando um novo modulo em package

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula100_introducao_packages.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula100_introducao_packages.py
@@ -1,16 +1,17 @@
-from sys import path
+# from sys import path
 
-import aula100_package.modulo
-from aula100_package.modulo import soma_do_modulo
-from aula100_package import modulo
+# import aula100_package.modulo
+# from aula100_package.modulo import soma_do_modulo
+# from aula100_package import modulo
 
-# Má pratica de importação
-from aula100_package.modulo import *
+# # Má pratica de importação
+# from aula100_package.modulo import *
 
-# print(*path, sep='\n')
+# # print(*path, sep='\n')
 
-print(soma_do_modulo(2, 3))
-print(aula100_package.modulo.soma_do_modulo(2, 3))
-print(modulo.soma_do_modulo(2, 3))
-print(variavel)
-print(nova_variavel)
+# print(soma_do_modulo(2, 3))
+# print(aula100_package.modulo.soma_do_modulo(2, 3))
+# print(modulo.soma_do_modulo(2, 3))
+# print(variavel)
+# print(nova_variavel)
+
