2e3731d416643efae55d6910ebf632a3bbb0caa3
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-17 22:51:13 -0300

    importando soma_do_modulo para dentro do main

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula100_introducao_packages.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula100_introducao_packages.py
@@ -1,6 +1,9 @@
 from sys import path
 
-import aula100_package
+import aula100_package.modulo
+from aula100_package.modulo import soma_do_modulo
 
 # print(*path, sep='\n')
 
+print(soma_do_modulo(2, 3))
+print(aula100_package.modulo.soma_do_modulo(2, 3))
