2cfd16cadcbcffd6c4b485941343b0b8b2c855a5
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-18 23:50:32 -0300

    formatando a saida dos dados para melhor entendimento

--- curso_python3_basico_avancado/exercicios/ex19.py
+++ curso_python3_basico_avancado/exercicios/ex19.py
@@ -6,11 +6,14 @@ from dados import produtos
 # Exercícios
 # Aumente os preços dos produtos a seguir em 10%
 # Gere novos_produtos por deep copy (cópia profunda)
+
 novo_produtos = copy.deepcopy(produtos)
 
+print('Produtos originais')
 print(*produtos, sep='\n')
 print()
-print(novo_produtos)
+print('Novos produtos')
+print(*novo_produtos, sep='\n')
 
 # Ordene os produtos por nome decrescente (do maior para menor)
 # Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
