d054f4dc0cde6354c13b9d08a6146b444b6439cd
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-08-19 00:03:23 -0300

    ordenando podutos pelo nome com função lambda

--- curso_python3_basico_avancado/exercicios/ex19.py
+++ curso_python3_basico_avancado/exercicios/ex19.py
@@ -18,9 +18,17 @@ print(*produtos, sep='\n')
 print()
 print('Novos produtos aumento de 10%')
 print(*novo_produtos, sep='\n')
+print()
 
 # Ordene os produtos por nome decrescente (do maior para menor)
 # Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
+produtos_ordenados_por_nome = sorted(
+    copy.deepcopy(produtos),
+    key=lambda p: p['nome']
+)
+
+print('Produtos ordenados por nome')
+print(*produtos_ordenados_por_nome, sep='\n')
 
 # Ordene os produtos por preco crescente (do menor para maior)
 # Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
