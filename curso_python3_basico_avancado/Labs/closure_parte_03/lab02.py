3b25f8785991cd6362fec5489fef2aedfb6e142d
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-08-24 17:41:01 -0300

    função anomima

--- curso_python3_basico_avancado/Labs/closure_parte_03/lab02.py
+++ curso_python3_basico_avancado/Labs/closure_parte_03/lab02.py
@@ -6,13 +6,3 @@ def div(x, y):
     """Função que soma dois valores"""
     return x / y
 
-
-# print(soma.__name__)
-# print(dir(soma))
-# print(soma.__doc__)
-# print(help(soma))
-
-try:
-    print(div(0, 0))
-except:
-    print(div.__name__)
