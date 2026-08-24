01c365cb942b366b710b19f8bff7fce8a48c9d3f
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-08-24 17:38:32 -0300

    nme da função com uma try pegando a excessao

--- curso_python3_basico_avancado/Labs/closure_parte_03/lab02.py
+++ curso_python3_basico_avancado/Labs/closure_parte_03/lab02.py
@@ -2,12 +2,17 @@
 Funções nomeadas em Python
 """
 
-def soma(x, y):
+def div(x, y):
     """Função que soma dois valores"""
-    return x + y
+    return x / y
 
 
 # print(soma.__name__)
 # print(dir(soma))
 # print(soma.__doc__)
 # print(help(soma))
+
+try:
+    print(div(0, 0))
+except:
+    print(div.__name__)
