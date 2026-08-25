8104271f693b6a19fc74c067e5e2931c7920b07f
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-08-25 20:27:55 -0300

    entendendo variaveis livres e nonlocal

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula101_freevars_nonlocal_global.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula101_freevars_nonlocal_global.py
@@ -1,6 +1,6 @@
 # Variáveis livres e nonlocal
 
-print(globals())
+# print(globals())
 
 # def fora(x):
 #     a = x
@@ -17,3 +17,20 @@ print(globals())
 
 # print(dentro())
 # print(dentro_2())
+
+def concatenar(string_inicial):
+    valor_final = string_inicial
+
+    def interna(valor_a_concatenar=''):
+        nonlocal valor_final
+        valor_final += valor_a_concatenar
+        return valor_final
+    return interna
+
+
+c = concatenar('a')
+print(c('b'))
+print(c('c'))
+print(c('d'))
+final = c()
+print(final)
