a98c2af7292d6f647cea9169ffbacfe0142ee147
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-08-25 17:21:46 -0300

    excutando uma closure

--- curso_python3_basico_avancado/Labs/closure_parte_03/lab04.py
+++ curso_python3_basico_avancado/Labs/closure_parte_03/lab04.py
@@ -1,11 +1,21 @@
 from pdb import set_trace
 
-def externa():
-    # set_trace()
-    def interna():
-        print(42)
+# Ola, Ahoy, Hello
+
+def externa(id):
+    dic = {'pt': 'olá', 'pi': 'Ahoy', 'en': 'Hello'}
+    def interna(nome):
+        print('{} {}'.format(dic[id], nome))
     return interna
 
 
-func = externa()
-func()
+func = externa('pt')
+func('Pedro')
+func('Maria')
+func('João')
+
+print()
+func = externa('pi')
+func('Pedro')
+func('Maria')
+func('João')
