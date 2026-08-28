7b3b7e2d9d069e5ec1576e2c26889df27a0fb500
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-08-28 17:15:59 -0300

    resolvendo o problema com apenas um modulo zip, unindo as listas

--- curso_python3_basico_avancado/exercicios/ex21.py
+++ curso_python3_basico_avancado/exercicios/ex21.py
@@ -9,13 +9,13 @@
 # Resultado
 # [('Salvador', 'BA'), ('Ubatuba', 'SP'), ('Belo Horizonte', 'MG')]
 
-def zipper(lista1, lista2):
-    intervalor_max = min(len(lista1), len(lista2))
-    return [
-        (lista1[i], lista2[i]) for i in range(intervalor_max)
-    ]
+# def zipper(lista1, lista2):
+#     intervalor_max = min(len(lista1), len(lista2))
+#     return [
+#         (lista1[i], lista2[i]) for i in range(intervalor_max)
+#     ]
 
 
 l1 = ['Salvador', 'Ubatuba', 'Belo Horizonte']
 l2 = ['BA', 'SP', 'MG', 'RJ']
-print(zipper(l1, l2))
+print(list(zip(l1, l2)))
