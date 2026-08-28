105860b9a730fa6cfa7bb627d26d1df144078476
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-08-28 17:10:25 -0300

    criando uma função que concatene duas listas de tamanhos diferentes

---
+++ curso_python3_basico_avancado/exercicios/ex21.py
@@ -0,0 +1,21 @@
+# Exercício - Unir listas
+# Crie uma função zipper (como o zipper de roupas)
+# O trabalho dessa função será unir duas
+# listas na ordem.
+# Use todos os valores da menor lista.
+# Ex.:
+# ['Salvador', 'Ubatuba', 'Belo Horizonte']
+# ['BA', 'SP', 'MG', 'RJ']
+# Resultado
+# [('Salvador', 'BA'), ('Ubatuba', 'SP'), ('Belo Horizonte', 'MG')]
+
+def zipper(lista1, lista2):
+    intervalor_max = min(len(lista1), len(lista2))
+    return [
+        i for i in range(intervalor_max)
+    ]
+
+
+l1 = [('Salvador', 'BA'), ('Ubatuba', 'SP'), ('Belo Horizonte', 'MG')]
+l2 = ['BA', 'SP', 'MG', 'RJ']
+print(zipper(l1, l2))
