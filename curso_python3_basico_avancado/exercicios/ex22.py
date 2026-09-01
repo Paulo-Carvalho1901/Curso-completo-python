8428e964a7be72177994601002160508c75e88c5
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-08-31 23:46:43 -0300

    segunda resolvução

--- curso_python3_basico_avancado/exercicios/ex22.py
+++ curso_python3_basico_avancado/exercicios/ex22.py
@@ -15,7 +15,14 @@ lista_soma  = [2, 4, 6, 8]
 lista_a     = [1, 2, 3, 4, 5, 6, 7]
 lista_b     = [1, 2, 3, 4]
 
+# lista_soma = []
+# for i in range(len(lista_b)):
+#     lista_soma.append(lista_a[i] + lista_b[i])
+# print(lista_soma)
+
+# Solução 2
+
 lista_soma = []
-for i in range(len(lista_b)):
+for i, _ in enumerate(lista_b):
     lista_soma.append(lista_a[i] + lista_b[i])
 print(lista_soma)
