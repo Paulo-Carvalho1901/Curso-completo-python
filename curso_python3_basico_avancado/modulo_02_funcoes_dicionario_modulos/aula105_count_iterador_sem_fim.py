b616ce68b5ce3079fd93f7982c375edb245067b4
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-09-02 00:38:05 -0300

    count iteravel e iterator verificando com hasattr

---
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula105_count_iterador_sem_fim.py
@@ -0,0 +1,15 @@
+# count é um iterador sem fim
+
+from itertools import count
+
+c1 = count()
+r1 = range(10)
+
+# print(next(c1))
+# print(next(c1))
+# print(next(c1))
+
+print('c1', hasattr(c1, '__iter__')) # Iteravel
+print('c1', hasattr(c1, '__next__')) # Iterator
+print('ri', hasattr(r1, '__iter__')) # Iteravel
+print('ri', hasattr(r1, '__next__')) # Iterator
