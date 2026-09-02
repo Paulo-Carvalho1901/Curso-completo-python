6b164fe51719d7caac845293ff5a1bcfe20fb537
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-09-02 00:18:54 -0300

    list comprehension de numeros

--- curso_python3_basico_avancado/Labs/list_comprehension/list_comprehention_04.py
+++ curso_python3_basico_avancado/Labs/list_comprehension/list_comprehention_04.py
@@ -1,7 +1,16 @@
 # Entendendo list comprehension
 
 numeros = [1, 2, 3, 4, 5]
-novos_numeros = numeros.copy()
+# novos_numeros = numeros.copy()
 
-numeros[0] = 20
+# numeros[0] = 20
+# print(novos_numeros)
+
+# Fazendo com List comprehension
+
+novos_numeros = [numero for numero in numeros] # agora minha lista não é alterada
+
+numeros[0] = 150
+
 print(novos_numeros)
+print(numeros)
