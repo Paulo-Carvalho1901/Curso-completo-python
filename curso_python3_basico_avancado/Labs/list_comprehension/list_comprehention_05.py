6e98831f90e728e72950ad3f0dd87aee6e6cedf1
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-09-02 20:49:04 -0300

    refatorando com funções

--- curso_python3_basico_avancado/Labs/list_comprehension/list_comprehention_05.py
+++ curso_python3_basico_avancado/Labs/list_comprehension/list_comprehention_05.py
@@ -1,9 +1,21 @@
 # Entendendo list comprehension
+def divisaoFn(x, y):
+    return x / y
+
+
+def multiplicacaoFn(x, y):
+    return x * y
+
+
+def potenciacaoFn(x, y):
+    return x ** y
+
+
 numeros = [1, 2, 3, 4, 5]
 
-divisao = [numero / 2 for numero in numeros]
-multiplicacao = [numero * 2 for numero in numeros]
-potenciacao = [numero ** 2 for numero in numeros]
+divisao = [divisaoFn(numero, 2) for numero in numeros]
+multiplicacao = [multiplicacaoFn(numero, 2) for numero in numeros]
+potenciacao = [potenciacaoFn(numero, 2) for numero in numeros]
 
 numeros[1] = 88
 print(numeros)
