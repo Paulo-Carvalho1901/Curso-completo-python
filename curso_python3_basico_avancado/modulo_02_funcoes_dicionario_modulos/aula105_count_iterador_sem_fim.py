9612bc009dbe771a330709f8906842cf21284a47
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-09-02 00:43:24 -0300

    inicializando com uma valor no count e no range

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula105_count_iterador_sem_fim.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula105_count_iterador_sem_fim.py
@@ -2,8 +2,8 @@
 
 from itertools import count
 
-c1 = count()
-r1 = range(10)
+c1 = count(10)
+r1 = range(10, 50)
 
 # print(next(c1))
 # print(next(c1))
@@ -14,7 +14,7 @@ print('c1', hasattr(c1, '__next__')) # Iterator
 print('ri', hasattr(r1, '__iter__')) # Iteravel
 print('ri', hasattr(r1, '__next__')) # Iterator
 
-print()
+print() 
 print('Count')
 for i in c1:
     if i > 25:
