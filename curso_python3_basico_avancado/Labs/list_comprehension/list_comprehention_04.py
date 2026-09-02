1ecbdb4b826eb3c7446baf013812bb131c4eaed2
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-09-02 00:23:35 -0300

    comentado o for e retornado para list comprehension

--- curso_python3_basico_avancado/Labs/list_comprehension/list_comprehention_04.py
+++ curso_python3_basico_avancado/Labs/list_comprehension/list_comprehention_04.py
@@ -8,11 +8,11 @@ numeros = [1, 2, 3, 4, 5]
 
 # Fazendo com List comprehension
 
-# novos_numeros = [numero for numero in numeros] # agora minha lista não é alterada
+novos_numeros = [numero for numero in numeros] # agora minha lista não é alterada
 
-novos_numeros = []
-for numero in numeros:
-    novos_numeros.append(numero)
+# novos_numeros = []
+# for numero in numeros:
+#     novos_numeros.append(numero)
     
 numeros[0] = 150
 
