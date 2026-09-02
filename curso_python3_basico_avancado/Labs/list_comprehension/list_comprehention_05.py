a5b9dc349dbfaba7db023d41cf8eb798cef0ecd5
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-09-02 18:49:48 -0300

    utilizandoa lista comprehensition para dividir os numeros da lista

--- curso_python3_basico_avancado/Labs/list_comprehension/list_comprehention_05.py
+++ curso_python3_basico_avancado/Labs/list_comprehension/list_comprehention_05.py
@@ -1,8 +1,8 @@
 # Entendendo list comprehension
 numeros = [1, 2, 3, 4, 5]
 
-novos_numeros = [numero for numero in numeros]
+divisao = [numero / 2 for numero in numeros]
 
 numeros[1] = 88
 print(numeros)
-print(novos_numeros)
+print(divisao)
