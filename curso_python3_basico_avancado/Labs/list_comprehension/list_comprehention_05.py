8d4bf010067ec5c697b5ebfd5384430cb6112a29
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-09-02 20:43:15 -0300

    add outras equações

--- curso_python3_basico_avancado/Labs/list_comprehension/list_comprehention_05.py
+++ curso_python3_basico_avancado/Labs/list_comprehension/list_comprehention_05.py
@@ -2,7 +2,11 @@
 numeros = [1, 2, 3, 4, 5]
 
 divisao = [numero / 2 for numero in numeros]
+multiplicacao = [numero * 2 for numero in numeros]
+potenciacao = [numero ** 2 for numero in numeros]
 
 numeros[1] = 88
 print(numeros)
 print(divisao)
+print(multiplicacao)
+print(potenciacao)
