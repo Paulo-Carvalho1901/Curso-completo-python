242f35a260c45782590ecb1da5ab68fb613a0272
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-09-02 00:42:25 -0300

    iterando sobre o range

--- curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula105_count_iterador_sem_fim.py
+++ curso_python3_basico_avancado/modulo_02_funcoes_dicionario_modulos/aula105_count_iterador_sem_fim.py
@@ -14,9 +14,15 @@ print('c1', hasattr(c1, '__next__')) # Iterator
 print('ri', hasattr(r1, '__iter__')) # Iteravel
 print('ri', hasattr(r1, '__next__')) # Iterator
 
+print()
 print('Count')
 for i in c1:
-    if i > 100:
+    if i > 25:
         break
 
+    print(i)
+
+print()
+print('Range')
+for i in r1:
     print(i)
