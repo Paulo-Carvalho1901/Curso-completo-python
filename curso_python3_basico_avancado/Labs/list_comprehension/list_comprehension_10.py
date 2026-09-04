5cb4ac69c8a4a69bb65158b514bda2da868caffe
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-09-04 00:45:19 -0300

    primeira forma de resolver esse problema

---
+++ curso_python3_basico_avancado/Labs/list_comprehension/list_comprehension_10.py
@@ -0,0 +1,8 @@
+# Imagine que voce precise criar
+# uma lista com 20 números aleatórios inteiros,
+# entre 0 e 20 como faria isso?
+
+# 1° forma
+lista_20_numeros_inteiros = list(range(0, 21))
+print(lista_20_numeros_inteiros)
+
