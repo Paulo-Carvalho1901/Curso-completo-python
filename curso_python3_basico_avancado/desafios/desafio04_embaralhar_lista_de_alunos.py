2ff4206455a81f5879c0f70460b9e87c001180a4
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-09-04 18:30:28 -0300

    criando um laço infinito

--- curso_python3_basico_avancado/desafios/desafio04_embaralhar_lista_de_alunos.py
+++ curso_python3_basico_avancado/desafios/desafio04_embaralhar_lista_de_alunos.py
@@ -5,14 +5,15 @@
 
 import random
 
-aluno1 = input('Digite o nome do primeiro aluno: ')
-aluno2 = input('Digite o nome do segundo aluno: ')
-aluno3 = input('Digite o nome do terceiro aluno: ')
-aluno4 = input('Digite o nome do quarto aluno: ')
+while True:
+    aluno1 = input('Digite o nome do primeiro aluno: ')
+    aluno2 = input('Digite o nome do segundo aluno: ')
+    aluno3 = input('Digite o nome do terceiro aluno: ')
+    aluno4 = input('Digite o nome do quarto aluno: ')
 
-lista_sorteados = [aluno1, aluno2, aluno3, aluno4]
+    lista_sorteados = [aluno1, aluno2, aluno3, aluno4]
 
-random.shuffle(lista_sorteados)
+    random.shuffle(lista_sorteados)
 
-for aluno in lista_sorteados:
-    print(aluno)
+    for aluno in lista_sorteados:
+        print(aluno)
