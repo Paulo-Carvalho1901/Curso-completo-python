436f83591b3fa570cbf3d1854db05c09668e385c
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-09-04 00:30:22 -0300

    criando o enunciado do desafio

--- curso_python3_basico_avancado/Labs/list_comprehension/list_comprehension_09.py
+++ curso_python3_basico_avancado/Labs/list_comprehension/list_comprehension_09.py
@@ -1,3 +1,6 @@
+# Pegue uma lista de nomes e
+# altere apenas a ultima letra para maiusculo
+
 nomes = ['paulo', 'andreia', 'davi', 'vitoria', 'daniel']
 
 novos_nomes = [f'{nome[:-1].lower()}{nome[-1].upper()}' for nome in nomes]
