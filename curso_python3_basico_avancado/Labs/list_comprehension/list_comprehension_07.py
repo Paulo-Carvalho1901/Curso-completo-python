d341ccd2892a826be215a228b9b3a5a3844754f8
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-09-04 00:03:27 -0300

    for aninhado com list comprehension

--- curso_python3_basico_avancado/Labs/list_comprehension/list_comprehension_07.py
+++ curso_python3_basico_avancado/Labs/list_comprehension/list_comprehension_07.py
@@ -1,3 +1,15 @@
+# for aninhado
 for x in range(1, 11):
     for y in range(1, 6):
         print(x, y)
+
+print()
+# for aninhado com list comprehension
+
+linhas_e_colunas = [
+    (x, y)
+    for x in range(1, 11)
+    for y in range(1, 6)
+]
+
+print(linhas_e_colunas)
