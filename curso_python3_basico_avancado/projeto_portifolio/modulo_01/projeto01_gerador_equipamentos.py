82afdbfcc36480f8a07146a7529e2591079e6183
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-09-04 17:48:11 -0300

    problema proposta do projeto 01 retornar apenas os hostnames

---
+++ curso_python3_basico_avancado/projeto_portifolio/modulo_01/projeto01_gerador_equipamentos.py
@@ -0,0 +1,19 @@
+# Gerenciador de Equipamentos
+
+# Problema
+# Imagine que você trabalha em uma equipe de infraestrutura.
+# Você recebeu uma lista de equipamentos:
+
+equipamentos = [
+    {"hostname": "SW-01", "ip": "192.168.1.10", "fabricante": "Cisco"},
+    {"hostname": "SW-02", "ip": "192.168.1.11", "fabricante": "Cisco"},
+    {"hostname": "SW-03", "ip": "192.168.1.12", "fabricante": "Huawei"},
+    {"hostname": "RTR-01", "ip": "192.168.1.1", "fabricante": "Cisco"},
+    {"hostname": "SW-04", "ip": "192.168.1.13", "fabricante": "Aruba"},
+]
+
+# Seu programa precisa responder algumas perguntas.
+# Desafio 1
+
+# Mostrar somente os hostnames:
+
