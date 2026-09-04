fa09949646ee6120117758b330208acaba3d302c
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-09-04 00:18:34 -0300

    dando um ponto a cada grupo de letras

--- curso_python3_basico_avancado/Labs/list_comprehension/list_comprehension_08.py
+++ curso_python3_basico_avancado/Labs/list_comprehension/list_comprehension_08.py
@@ -1,9 +1,9 @@
 string = 'Paulo Carvalho'
 numeros_de_letras = 3
 
-nova_string = [
+nova_string = '.'.join([
     string[indice:indice + numeros_de_letras] 
     for indice in range(0, len(string), numeros_de_letras)
-]
+])
 
 print(nova_string)
