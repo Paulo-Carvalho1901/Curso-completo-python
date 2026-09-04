37eb8cd8050fd5d79cdc48d4b643f8034089a886
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-09-04 00:51:59 -0300

    2segunda forma

--- curso_python3_basico_avancado/Labs/list_comprehension/list_comprehension_10.py
+++ curso_python3_basico_avancado/Labs/list_comprehension/list_comprehension_10.py
@@ -2,7 +2,16 @@
 # uma lista com 20 números aleatórios inteiros,
 # entre 0 e 20 como faria isso?
 
-# 1° forma
-lista_20_numeros_inteiros = list(range(0, 21))
-print(lista_20_numeros_inteiros)
+import random
 
+# 1° forma
+# lista_20_numeros_inteiros = random.randint(0,)
+# print(lista_20_numeros_inteiros)
+
+# 2° forma
+numeros = []
+for i in range(20):
+    numeros_randomicos = random.randint(1, 10)
+    numeros.append(numeros_randomicos)
+
+print(numeros)
