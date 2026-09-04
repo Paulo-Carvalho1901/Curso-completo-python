79d3d7cf6219b6209e802d34f6938b1d3f9321e9
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-09-04 13:23:06 -0300

    estrutura de list comprehension

--- curso_python3_basico_avancado/Labs/list_comprehension/list_comprehension_10.py
+++ curso_python3_basico_avancado/Labs/list_comprehension/list_comprehension_10.py
@@ -8,10 +8,15 @@ import random
 # lista_20_numeros_inteiros = random.randint(0, 20)
 # print(lista_20_numeros_inteiros)
 
-# 2° forma
+# 1° forma de resolver
 numeros = []
 for i in range(20):
     numeros_randomicos = random.randint(1, 10)
     numeros.append(numeros_randomicos)
 
 print(numeros)
+
+# 2° forma de resolver
+# List comprehension
+# Estrutura da lista comprehension
+# [o que você quer na lista] for i in range(n)
