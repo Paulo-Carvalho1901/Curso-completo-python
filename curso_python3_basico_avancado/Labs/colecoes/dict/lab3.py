dca32b7954bac6476ea773cb01d89b149f24148a
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-09-06 21:21:54 -0300

    alterando valor do dict pela chave

--- curso_python3_basico_avancado/Labs/colecoes/dict/lab3.py
+++ curso_python3_basico_avancado/Labs/colecoes/dict/lab3.py
@@ -2,7 +2,15 @@
 
 pessoa = {}
 
+chave = 'nome_completo'
+
+
 # add um chave e valor no dict
-pessoa['nome'] = 'Paulo'
+pessoa[chave] = 'Paulo Carvalho'
+
+print(pessoa[chave])
+
+# Alterando valor da dict pela chave
+pessoa[chave] = 'Andreia Cristina'
 
 print(pessoa)
