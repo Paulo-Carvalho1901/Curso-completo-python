15db2266c91a1b116fb67a5abd449c5d3c7b77d2
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-09-06 21:24:07 -0300

    apagando uma chave do dict

--- curso_python3_basico_avancado/Labs/colecoes/dict/lab3.py
+++ curso_python3_basico_avancado/Labs/colecoes/dict/lab3.py
@@ -7,10 +7,15 @@ chave = 'nome_completo'
 
 # add um chave e valor no dict
 pessoa[chave] = 'Paulo Carvalho'
+pessoa['sobrenome'] = 'Carvalho'
+
 
 print(pessoa[chave])
 
 # Alterando valor da dict pela chave
 pessoa[chave] = 'Andreia Cristina'
+
+# Apagando uma chave
+del pessoa['sobrenome']
 
 print(pessoa)
